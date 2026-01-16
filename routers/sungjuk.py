1921fd5e1a0f5ea2a37ad1aa7074adf9a2dda438
globalit-tutor <zzyzigy250508@gmail.com>
2026-01-16 12:17:54 +0900

    라우터 기반 성적처리 앱 성적 등록 구현

--- routers/sungjuk.py
+++ routers/sungjuk.py
@@ -34,13 +34,23 @@ async def sungjuk_list(request: Request):
 
 @router.get("/new", response_class=HTMLResponse)
 async def sungjuk_newform(request: Request):
-    pass
+    return templates.TemplateResponse("sungjuk/sungjuk_new.html", {"request": request})
 
 
 @router.post("/new", response_class=HTMLResponse)
 async def sungjuk_new(request: Request, name: str = Form(...),
        kor: int = Form(...), eng: int = Form(...), mat: int = Form(...)):
-    pass
+    # 성적 처리 (총점/평균/학점 계산)
+    tot, avg, grd = compute_sungjuk(kor, eng, mat)
+
+    async with aiosqlite.connect(SungJukDB_NAME) as db:
+        await db.execute(
+            """insert into sungjuk (name,kor,eng,mat,tot,avg,grd) 
+               values (?, ?, ?, ?, ?, ?, ?)""",
+            (name, kor, eng, mat, tot, avg, grd))
+        await db.commit()
+
+    return RedirectResponse(url="/sungjuk/list", status_code=303)
 
 
 @router.get("/{sjno}", response_class=HTMLResponse)
@@ -63,3 +73,18 @@ async def sungjuk_edit(request: Request, sjno: int,
     kor: int = Form(...), eng: int = Form(...), mat: int = Form(...)):
     pass
 
+
+def compute_sungjuk(kor, eng, mat):
+    """
+    성적 데이터에 대한 총점,평균,학점 처리
+
+    :return:
+    """
+    tot = kor + eng + mat
+    avg = tot / 3
+    grd = ('A' if (avg >= 90) else
+           'B' if (avg >= 80) else
+           'C' if (avg >= 70) else
+           'D' if (avg >= 60) else 'F')
+
+    return tot, avg, grd
